e079196ccfdd1865d330728f931d3984ee72f068
Mallika <dev.highlightapps@gmail.com>
2023-04-19 19:34:40 +0530

    first commit

---
+++ src/app/data-type.ts
@@ -0,0 +1,18 @@
+export interface signUp{
+    name:string,
+    email:string,
+    password:string
+}
+export interface login{
+    email:String,
+    password:String
+}
+export interface product{
+    name:String,
+    price:number,
+    category:string,
+    color:string,
+    image:string,
+    description:string,
+    id:number
+}
